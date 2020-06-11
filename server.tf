2c344b857b488908f6fba244221c41134989e198
Kevin Cochran <kcochran@hashicorp.com>
2020-06-11 12:45:30 -0400

    update

--- server.tf
+++ server.tf
@@ -160,10 +160,27 @@ resource "azurerm_virtual_machine" "azure-vm" {
         storage_uri = azurerm_storage_account.blob-store.primary_blob_endpoint
     }
 
+    identity {
+        type = "SystemAssigned"
+    }
+
     tags = {
         environment = "Dev"
         Key = "DoNotDelete"
     }
+}
+
+data "azurerm_subscription" "current" {}
+
+data "azurerm_role_definition" "contributor" {
+    name = "Contributor"
+}
+
+resource "azurerm_role_assignment" "example" {
+    name               = azurerm_virtual_machine.azure-vm.name
+    scope              = data.azurerm_subscription.current.id
+    role_definition_id = "${data.azurerm_subscription.current.id}${data.azurerm_role_definition.contributor.id}"
+    principal_id       = azurerm_virtual_machine.azure-vm.identity[0]["principal_id"]
 }
 
 data "azurerm_public_ip" "public-ip" {
