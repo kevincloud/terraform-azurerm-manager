10e0047d67ccdf70f0fde71897be969b032862c8
Kevin Cochran <kcochran@hashicorp.com>
2020-06-15 19:16:58 -0400

    update

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "ssh" {
     value = "ssh ubuntu@${data.azurerm_public_ip.public-ip.ip_address}"
 }
+
+output "api" {
+    value = "http://${aws_route53_record.sentinel-data.fqdn}:8080/"
+}
+
+output "web" {
+    value = "http://${aws_route53_record.sentinel-data.fqdn}/"
+}
