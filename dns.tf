feb692e055a738e90ec8919854959b1aa7559cae
Kevin Cochran <kcochran@hashicorp.com>
2020-06-15 22:05:37 -0400

    update

--- dns.tf
+++ dns.tf
@@ -2,10 +2,10 @@ data "azurerm_resource_group" "static-rg" {
     name = "kevinc-static-resources"
 }
 
-# resource "azurerm_dns_a_record" "sentinel-data" {
-#   name                = "sentinel-data"
-#   zone_name           = var.dns_zone
-#   resource_group_name = data.azurerm_resource_group.static-rg.name
-#   ttl                 = 300
-#   records             = [data.azurerm_public_ip.public-ip.ip_address]
-# }
+resource "azurerm_dns_a_record" "sentinel-data" {
+  name                = "sentinel-data"
+  zone_name           = var.dns_zone
+  resource_group_name = data.azurerm_resource_group.static-rg.name
+  ttl                 = 300
+  records             = [data.azurerm_public_ip.public-ip.ip_address]
+}
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-# output "ssh" {
-#     value = "ssh ubuntu@${data.azurerm_public_ip.public-ip.ip_address}"
-# }
+output "ssh" {
+    value = "ssh ubuntu@${data.azurerm_public_ip.public-ip.ip_address}"
+}
 
-# output "api" {
-#     value = "http://${azurerm_dns_a_record.sentinel-data.fqdn}:8080/"
-# }
+output "api" {
+    value = "http://${azurerm_dns_a_record.sentinel-data.fqdn}:8080/"
+}
 
-# output "web" {
-#     value = "http://${azurerm_dns_a_record.sentinel-data.fqdn}/"
-# }
+output "web" {
+    value = "http://${azurerm_dns_a_record.sentinel-data.fqdn}/"
+}
