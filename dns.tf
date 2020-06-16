6af158189d25239e8a6c5481e46458e72cee7580
Kevin Cochran <kcochran@hashicorp.com>
2020-06-15 20:41:59 -0400

    update

--- dns.tf
+++ dns.tf
@@ -1,23 +1,10 @@
-# data "aws_route53_zone" "primary" {
-#     zone_id         = var.zone_id
-#     private_zone = true
-# }
-
-# resource "aws_route53_record" "sentinel-data" { 
-#     zone_id = data.aws_route53_zone.primary.zone_id
-#     name = "sentinel-data.${data.aws_route53_zone.primary.name}"
-#     type = "A"
-#     ttl = "300"
-#     records = [data.azurerm_public_ip.public-ip.ip_address]
-# }
-
 data "azurerm_resource_group" "static-rg" {
     name = "kevinc-static-resources"
 }
 
 resource "azurerm_dns_a_record" "sentinel-data" {
   name                = "sentinel-data"
-  zone_name           = "kcochran.azure.hashidemos.io"
+  zone_name           = var.dns_zone
   resource_group_name = data.azurerm_resource_group.static-rg.name
   ttl                 = 300
   records             = [data.azurerm_public_ip.public-ip.ip_address]
