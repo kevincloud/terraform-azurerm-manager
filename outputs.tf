926bc2bf2b975ecb73ac8ab458d31ea1b11bf628
Kevin Cochran <kcochran@hashicorp.com>
2020-06-23 16:36:43 -0400

    update

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "B-api" {
 output "C-web" {
     value = "http://sentinel-data.${var.dns_zone}/"
 }
+
+output "D-acctkey" {
+    value = azurerm_cosmosdb_account.cosmosdb.primary_master_key
+}
