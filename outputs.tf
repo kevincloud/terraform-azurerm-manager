28a034740d624a5397610d0c97c5c755615b3814
Kevin Cochran <kcochran@hashicorp.com>
2020-07-09 14:31:07 -0400

    change login name in output to variable

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "A-ssh" {
-    value = "ssh ubuntu@${data.azurerm_public_ip.public-ip.ip_address}"
+    value = "ssh ${var.linux_user}@${data.azurerm_public_ip.public-ip.ip_address}"
 }
 
 output "B-api" {
@@ -10,6 +10,6 @@ output "C-web" {
     value = "http://sentinel-data.${var.dns_zone}/"
 }
 
-output "D-acctkey" {
-    value = azurerm_cosmosdb_account.cosmosdb.primary_master_key
-}
+# output "D-acctkey" {
+#     value = azurerm_cosmosdb_account.cosmosdb.primary_master_key
+# }
